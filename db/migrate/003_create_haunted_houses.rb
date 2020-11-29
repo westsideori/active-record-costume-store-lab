f3fef7237e9c417108906f4c2eaf7dcedba39584
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-29 12:51:38 -0500

    finished active record costume store

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,18 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.datetime :opening_date
+            t.datetime :closing_date
+            t.string :description
+        end
+    end
+end
